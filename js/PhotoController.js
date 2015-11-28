82bbd59f11147318451b78dacd81a54bcdbbe33b
Mike Reddy <Mike@Mikes-MacBook-Pro.local>
2015-11-28 21:49:03 +0900

    Updated photos.

--- js/PhotoController.js
+++ js/PhotoController.js
@@ -1,36 +1,23 @@
-angular.module('demo1', ['bootstrapLightbox']);
-
-angular.module('app').controller('GalleryCtrl', function ($scope, Lightbox) {
-
-
-
-// app.controller('PhotoController', ['$scope', function($scope) {
+app.controller('PhotoController', ['$scope', function($scope) {
   
   // Appetizer data.
   $scope.images = [
     {
       large: 'img/photography/minato-mirai-large-1.jpg',
-      small: 'img/photography/minato-mirai-large-1.jpg'
+      small: 'img/photography/minato-mirai-small-1.jpg'
     },
     {
-      large: 'img/photography/minato-mirai-large-2.jpg',
-      small: 'img/photography/minato-mirai-small-2.jpg'
-    },
-    {
-      large: 'img/photography/minato-mirai-small-3.jpg',
+      large: 'img/photography/minato-mirai-large-3.jpg',
       small: 'img/photography/minato-mirai-small-3.jpg'
     },
     {
-      large: 'img/photography/minato-mirai-small-4.jpg',
+      large: 'img/photography/minato-mirai-large-4.jpg',
       small: 'img/photography/minato-mirai-small-4.jpg'
     },
     {
-      large: 'img/photography/minato-mirai-small-5.jpg',
+      large: 'img/photography/minato-mirai-large-5.jpg',
       small: 'img/photography/minato-mirai-small-5.jpg'
-    }
-  ];
-
-  $scope.enoshima = [
+    },
     {
       large: 'img/photography/enoshima-large-1.jpg',
       small: 'img/photography/enoshima-small-1.jpg'
@@ -47,17 +34,7 @@ angular.module('app').controller('GalleryCtrl', function ($scope, Lightbox) {
       large: 'img/photography/enoshima-large-4.jpg',
       small: 'img/photography/enoshima-small-4.jpg'
     },
-    {
-      large: 'img/photography/enoshima-large-5.jpg',
-      small: 'img/photography/enoshima-small-5.jpg'
-    },
-    {
-      large: 'img/photography/enoshima-large-2.jpg',
-      small: 'img/photography/enoshima-small-2.jpg'
-    }
-  ];
 
-  $scope.tachikawa = [
     {
       large: 'img/photography/showa-kinen-kouen-large-1.jpg',
       small: 'img/photography/showa-kinen-kouen-small-1.jpg'
@@ -75,75 +52,104 @@ angular.module('app').controller('GalleryCtrl', function ($scope, Lightbox) {
       small: 'img/photography/showa-kinen-kouen-small-4.jpg'
     },
     {
-      large: 'img/photography/showa-kinen-kouen-large-10.jpg',
-      small: 'img/photography/showa-kinen-kouen-small-10.jpg'
-    },
-    {
-      large: 'img/photography/showa-kinen-kouen-large-6.jpg',
-      small: 'img/photography/showa-kinen-kouen-small-6.jpg'
-    },
-    {
-      large: 'img/photography/showa-kinen-kouen-large-7.jpg',
-      small: 'img/photography/showa-kinen-kouen-small-7.jpg'
-    },
-    {
-      large: 'img/photography/showa-kinen-kouen-large-8.jpg',
-      small: 'img/photography/showa-kinen-kouen-small-8.jpg'
-    },
-    {
       large: 'img/photography/showa-kinen-kouen-large-2.jpg',
       small: 'img/photography/showa-kinen-kouen-small-2.jpg'
     },
     {
       large: 'img/photography/showa-kinen-kouen-large-5.jpg',
       small: 'img/photography/showa-kinen-kouen-small-5.jpg'
-    }
-  ];
-
-$scope.illumination = [
-    {
-      large: 'img/photography/illumination-large-1.jpg',
-      small: 'img/photography/illumination-small-1.jpg'
     },
     {
+      large: 'img/photography/enoshima-large-2.jpg',
+      small: 'img/photography/enoshima-small-2.jpg'
+    },
+    {
+      large: 'img/photography/seijin-large-7.jpg',
+      small: 'img/photography/seijin-small-7.jpg'
+    },
+    {
+      large: 'img/photography/misc-large-1.jpg',
+      small: 'img/photography/misc-small-1.jpg'
+    },
+    {
+      large: 'img/photography/misc-large-2.jpg',
+      small: 'img/photography/misc-small-2.jpg'
+    },
+    {
+      large: 'img/photography/misc-large-3.jpg',
+      small: 'img/photography/misc-small-3.jpg'
+    },
+    {
+      large: 'img/photography/misc-large-4.jpg',
+      small: 'img/photography/misc-small-4.jpg'
+    },
+    {
+      large: 'img/photography/misc-large-5.jpg',
+      small: 'img/photography/misc-small-5.jpg'
+    },
+    {
+      large: 'img/photography/misc-large-6.jpg',
+      small: 'img/photography/misc-small-6.jpg'
+    },
+    {
+      large: 'img/photography/misc-large-7.jpg',
+      small: 'img/photography/misc-small-7.jpg'
+    },
+        {
+      large: 'img/photography/illumination-large-15.jpg',
+      small: 'img/photography/illumination-small-15.jpg'
+    },    {
+      large: 'img/photography/misc-large-11.jpg',
+      small: 'img/photography/misc-small-11.jpg'
+    },    {
+      large: 'img/photography/misc-large-12.jpg',
+      small: 'img/photography/misc-small-12.jpg'
+    },    {
+      large: 'img/photography/misc-large-13.jpg',
+      small: 'img/photography/misc-small-13.jpg'
+    },    {
+      large: 'img/photography/illumination-large-1.jpg',
+      small: 'img/photography/illumination-small-1.jpg'
+    },    {
+      large: 'img/photography/illumination-large-2.jpg',
+      small: 'img/photography/illumination-small-2.jpg'
+    },    {
+      large: 'img/photography/illumination-large-3.jpg',
+      small: 'img/photography/illumination-small-3.jpg'
+    },{
+      large: 'img/photography/illumination-large-4.jpg',
+      small: 'img/photography/illumination-small-4.jpg'
+    },    {
+      large: 'img/photography/illumination-large-5.jpg',
+      small: 'img/photography/illumination-small-5.jpg'
+    },    {
+      large: 'img/photography/illumination-large-6.jpg',
+      small: 'img/photography/illumination-small-6.jpg'
+    },{
+      large: 'img/photography/illumination-large-7.jpg',
+      small: 'img/photography/illumination-small-7.jpg'
+    },    {
+      large: 'img/photography/illumination-large-8.jpg',
+      small: 'img/photography/illumination-small-8.jpg'
+    },    {
       large: 'img/photography/illumination-large-9.jpg',
       small: 'img/photography/illumination-small-9.jpg'
     },
-    {
-      large: 'img/photography/illumination-large-3.jpg',
-      small: 'img/photography/illumination-small-3.jpg'
-    },
-    {
-      large: 'img/photography/illumination-large-4.jpg',
-      small: 'img/photography/illumination-small-4.jpg'
-    },
-    {
+{
       large: 'img/photography/illumination-large-10.jpg',
       small: 'img/photography/illumination-small-10.jpg'
+    },   
+     {
+      large: 'img/photography/illumination-large-12.jpg',
+      small: 'img/photography/illumination-small-12.jpg'
     },
-    {
-      large: 'img/photography/illumination-large-6.jpg',
-      small: 'img/photography/illumination-small-6.jpg'
-    },
-    {
-      large: 'img/photography/illumination-large-7.jpg',
-      small: 'img/photography/illumination-small-7.jpg'
-    },
-    {
-      large: 'img/photography/illumination-large-8.jpg',
-      small: 'img/photography/illumination-small-8.jpg'
-    },
-    {
-      large: 'img/photography/illumination-large-2.jpg',
-      small: 'img/photography/illumination-small-2.jpg'
-    },
-    {
-      large: 'img/photography/illumination-large-5.jpg',
-      small: 'img/photography/illumination-small-5.jpg'
+{
+      large: 'img/photography/illumination-large-13.jpg',
+      small: 'img/photography/illumination-small-13.jpg'
+    },    {
+      large: 'img/photography/illumination-large-14.jpg',
+      small: 'img/photography/illumination-small-14.jpg'
     }
-  ];
-  $scope.openLightboxModal = function (index) {
-    Lightbox.openModal($scope.images, index);
-  };
 
-});
+  ];
+}]);
