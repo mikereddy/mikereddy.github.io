2d83d416a68b31e0ce51d059424e732792ebd848
Mike Reddy <Mike@Mikes-MacBook-Pro.local>
2015-11-28 19:48:52 +0900

    Photo updates.

--- js/PhotoController.js
+++ js/PhotoController.js
@@ -1,98 +1,149 @@
-app.controller('PhotoController', ['$scope', function($scope) {
+angular.module('demo1', ['bootstrapLightbox']);
+
+angular.module('app').controller('GalleryCtrl', function ($scope, Lightbox) {
+
+
+
+// app.controller('PhotoController', ['$scope', function($scope) {
   
   // Appetizer data.
-  $scope.minatomirai = [
+  $scope.images = [
     {
-      large: 'img/photography/minatomirai/MinatoMiraiLarge-1.jpg',
-      small: 'img/photography/minatomirai/MinatoMiraiLarge-1.jpg'
+      large: 'img/photography/minato-mirai-large-1.jpg',
+      small: 'img/photography/minato-mirai-large-1.jpg'
     },
     {
-      large: 'img/photography/minatomirai/MinatoMiraiLarge-1.jpg',
-      small: 'img/photography/minatomirai/MinatoMiraiSmall-1.jpg'
+      large: 'img/photography/minato-mirai-large-2.jpg',
+      small: 'img/photography/minato-mirai-small-2.jpg'
     },
     {
-      large: 'img/photography/minatomirai/minatomirai-small-3.jpg',
-      small: 'img/photography/minatomirai/minatomirai-small-3.jpg'
+      large: 'img/photography/minato-mirai-small-3.jpg',
+      small: 'img/photography/minato-mirai-small-3.jpg'
     },
     {
-      large: 'img/photography/minatomirai/minatomirai-small-4.jpg',
-      small: 'img/photography/minatomirai/minatomirai-small-4.jpg'
+      large: 'img/photography/minato-mirai-small-4.jpg',
+      small: 'img/photography/minato-mirai-small-4.jpg'
     },
     {
-      large: 'img/photography/minatomirai/minatomirai-small-5.jpg',
-      small: 'img/photography/minatomirai/minatomirai-small-5.jpg'
+      large: 'img/photography/minato-mirai-small-5.jpg',
+      small: 'img/photography/minato-mirai-small-5.jpg'
     }
   ];
 
   $scope.enoshima = [
     {
-      large: 'img/photography/enoshima/enoshima-large-1.jpg',
-      small: 'img/photography/enoshima/enoshima-small-1.jpg'
+      large: 'img/photography/enoshima-large-1.jpg',
+      small: 'img/photography/enoshima-small-1.jpg'
     },
     {
-      large: 'img/photography/enoshima/enoshima-large-6.jpg',
-      small: 'img/photography/enoshima/enoshima-small-6.jpg'
+      large: 'img/photography/enoshima-large-6.jpg',
+      small: 'img/photography/enoshima-small-6.jpg'
     },
     {
-      large: 'img/photography/enoshima/enoshima-large-3.jpg',
-      small: 'img/photography/enoshima/enoshima-small-3.jpg'
+      large: 'img/photography/enoshima-large-3.jpg',
+      small: 'img/photography/enoshima-small-3.jpg'
     },
     {
-      large: 'img/photography/enoshima/enoshima-large-4.jpg',
-      small: 'img/photography/enoshima/enoshima-small-4.jpg'
+      large: 'img/photography/enoshima-large-4.jpg',
+      small: 'img/photography/enoshima-small-4.jpg'
     },
     {
-      large: 'img/photography/enoshima/enoshima-large-5.jpg',
-      small: 'img/photography/enoshima/enoshima-small-5.jpg'
+      large: 'img/photography/enoshima-large-5.jpg',
+      small: 'img/photography/enoshima-small-5.jpg'
     },
     {
-      large: 'img/photography/enoshima/enoshima-large-2.jpg',
-      small: 'img/photography/enoshima/enoshima-small-2.jpg'
+      large: 'img/photography/enoshima-large-2.jpg',
+      small: 'img/photography/enoshima-small-2.jpg'
     }
   ];
 
   $scope.tachikawa = [
     {
-      large: 'img/photography/showakinenkouen/showakinenkouenlarge-1.jpg',
-      small: 'img/photography/showakinenkouen/showakinenkouensmall-1.jpg'
+      large: 'img/photography/showa-kinen-kouen-large-1.jpg',
+      small: 'img/photography/showa-kinen-kouen-small-1.jpg'
     },
     {
-      large: 'img/photography/showakinenkouen/showakinenkouenlarge-9.jpg',
-      small: 'img/photography/showakinenkouen/showakinenkouensmall-9.jpg'
+      large: 'img/photography/showa-kinen-kouen-large-9.jpg',
+      small: 'img/photography/showa-kinen-kouen-small-9.jpg'
     },
     {
-      large: 'img/photography/showakinenkouen/showakinenkouenlarge-3.jpg',
-      small: 'img/photography/showakinenkouen/showakinenkouensmall-3.jpg'
+      large: 'img/photography/showa-kinen-kouen-large-3.jpg',
+      small: 'img/photography/showa-kinen-kouen-small-3.jpg'
     },
     {
-      large: 'img/photography/showakinenkouen/showakinenkouenlarge-4.jpg',
-      small: 'img/photography/showakinenkouen/showakinenkouensmall-4.jpg'
+      large: 'img/photography/showa-kinen-kouen-large-4.jpg',
+      small: 'img/photography/showa-kinen-kouen-small-4.jpg'
     },
     {
-      large: 'img/photography/showakinenkouen/showakinenkouenlarge-10.jpg',
-      small: 'img/photography/showakinenkouen/showakinenkouensmall-10.jpg'
+      large: 'img/photography/showa-kinen-kouen-large-10.jpg',
+      small: 'img/photography/showa-kinen-kouen-small-10.jpg'
     },
     {
-      large: 'img/photography/showakinenkouen/showakinenkouenlarge-6.jpg',
-      small: 'img/photography/showakinenkouen/showakinenkouensmall-6.jpg'
+      large: 'img/photography/showa-kinen-kouen-large-6.jpg',
+      small: 'img/photography/showa-kinen-kouen-small-6.jpg'
     },
     {
-      large: 'img/photography/showakinenkouen/showakinenkouenlarge-7.jpg',
-      small: 'img/photography/showakinenkouen/showakinenkouensmall-7.jpg'
+      large: 'img/photography/showa-kinen-kouen-large-7.jpg',
+      small: 'img/photography/showa-kinen-kouen-small-7.jpg'
     },
     {
-      large: 'img/photography/showakinenkouen/showakinenkouenlarge-8.jpg',
-      small: 'img/photography/showakinenkouen/showakinenkouensmall-8.jpg'
+      large: 'img/photography/showa-kinen-kouen-large-8.jpg',
+      small: 'img/photography/showa-kinen-kouen-small-8.jpg'
     },
     {
-      large: 'img/photography/showakinenkouen/showakinenkouenlarge-2.jpg',
-      small: 'img/photography/showakinenkouen/showakinenkouensmall-2.jpg'
+      large: 'img/photography/showa-kinen-kouen-large-2.jpg',
+      small: 'img/photography/showa-kinen-kouen-small-2.jpg'
     },
     {
-      large: 'img/photography/showakinenkouen/showakinenkouenlarge-5.jpg',
-      small: 'img/photography/showakinenkouen/showakinenkouensmall-5.jpg'
+      large: 'img/photography/showa-kinen-kouen-large-5.jpg',
+      small: 'img/photography/showa-kinen-kouen-small-5.jpg'
     }
   ];
 
+$scope.illumination = [
+    {
+      large: 'img/photography/illumination-large-1.jpg',
+      small: 'img/photography/illumination-small-1.jpg'
+    },
+    {
+      large: 'img/photography/illumination-large-9.jpg',
+      small: 'img/photography/illumination-small-9.jpg'
+    },
+    {
+      large: 'img/photography/illumination-large-3.jpg',
+      small: 'img/photography/illumination-small-3.jpg'
+    },
+    {
+      large: 'img/photography/illumination-large-4.jpg',
+      small: 'img/photography/illumination-small-4.jpg'
+    },
+    {
+      large: 'img/photography/illumination-large-10.jpg',
+      small: 'img/photography/illumination-small-10.jpg'
+    },
+    {
+      large: 'img/photography/illumination-large-6.jpg',
+      small: 'img/photography/illumination-small-6.jpg'
+    },
+    {
+      large: 'img/photography/illumination-large-7.jpg',
+      small: 'img/photography/illumination-small-7.jpg'
+    },
+    {
+      large: 'img/photography/illumination-large-8.jpg',
+      small: 'img/photography/illumination-small-8.jpg'
+    },
+    {
+      large: 'img/photography/illumination-large-2.jpg',
+      small: 'img/photography/illumination-small-2.jpg'
+    },
+    {
+      large: 'img/photography/illumination-large-5.jpg',
+      small: 'img/photography/illumination-small-5.jpg'
+    }
+  ];
+  $scope.openLightboxModal = function (index) {
+    Lightbox.openModal($scope.images, index);
+  };
 
-}]);
+});
